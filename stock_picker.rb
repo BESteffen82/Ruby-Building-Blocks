f767652764891c4917afcd58b36e14644ed68231
Brent Steffen <BESteffen82@gmail.com>
2022-05-19 17:15:06 -0400

    Add buy and sell price variables

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,9 +1,10 @@
 require "pry-byebug"
 
-def stock_picker(numbers)
+def stock_picker(prices)
+  buy_price = prices.each_with_index.select{|price, index| index != (prices.length - 1)}.map{|price| price[0]}		
+	sell_price = prices.each_with_index.select{|price,index| index != 0}.map{|price| price[0]}
 
 	binding.pry
-
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
