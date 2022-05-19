b2dfa447eaeab172d13cb53028c111a76d4def9d
Brent Steffen <BESteffen82@gmail.com>
2022-05-19 15:11:03 -0400

    Final code

--- substrings.rb
+++ substrings.rb
@@ -1,2 +1,9 @@
-require "pry-byebug"
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+def substrings(string, dictionary)
+	words = dictionary.map{|word| word if string.downcase.include?(word) == true}.compact
+	count = words.map{|word| string.scan(word).length}
+	word_count = words.zip(count).to_h	
+end	
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)	
