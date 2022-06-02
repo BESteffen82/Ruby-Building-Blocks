061d499caa5f0800dec0f98c27d1addbbd0131c5
Brent Steffen <BESteffen82@gmail.com>
2022-06-02 17:21:31 -0400

    Final code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,21 @@
-require "pry-byebug"
+def bubble_sort(array)
+	n = array.length
+	swapped = true	
+
+	until !swapped
+		swapped = false
+		(1...n).each do |index|
+			if array[index-1] > array[index]
+				array[index-1], array[index] = array[index], array[index-1]
+				swapped = true
+			end
+		end
+	end
+
+	array
+	p array	
+end
+
+
+
+bubble_sort([4,3,78,2,0,2])
